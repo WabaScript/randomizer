76798bf00275dedccf3f44eaf3400607fca99a8f
wabascript <rjwahba@gmail.com>
2021-09-12 11:01:32 -0400

    roulette picker

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
     createTags(e.target.value)
+
+    if (e.key === 'Enter') {
+        setTimeout(() => {
+            e.target.value = ''
+        }, 10)
+
+        randomSelection()
+    }
 })
 
 function createTags(input) {
@@ -16,4 +24,43 @@ function createTags(input) {
         tagElement.innerText = tag
         tagsElement.appendChild(tagElement)
     });
+}
+
+function randomSelection() {
+    const highlightAmount = 20
+
+    // roulette highlight and unhighlight effect
+    const interval = setInterval(() => {
+        const randomTag = pickRandomTag()
+        highlightTag(randomTag)
+
+        setTimeout(() => {
+            unhighlightTag(randomTag)
+        }, 100)
+    }, 100);
+
+    // stops roulette effect and selects a tag
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(() => {
+            const randomTag = pickRandomTag()
+
+            highlightTag(randomTag)
+        }, 100)
+    }, highlightAmount * 100)
+}
+
+function pickRandomTag() {
+    const tags = document.querySelectorAll('.tag')
+    // pick random tag from Node list
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag) {
+    tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag) {
+    tag.classList.remove('highlight')
 }
